f71bc0b0b2d4a3f15454ce79a081018d15759e36
Ahmed K Madani <ahmedkamal543@gmail.com>
2022-03-19 15:06:54 +0300

    update read and delete

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -22,4 +24,19 @@ func main() {
 
 	//Create
 	db.Create(&Product{Code: "D42", Price: 100})
+
+	//Read
+	var product Product
+	fmt.Println(db.First(&product, 1))                 //find product with integer primary key
+	fmt.Println(db.First(&product, "code = ?", "D42")) //find product with code D42
+
+	//Update - update product price to 200
+	db.Model(&product).Update("Price", 200)
+
+	db.Model(&product).Updates(Product{Price: 200, Code: "F42"})
+	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "C90"})
+
+	//Delete Product
+	db.Delete(&product, 1)
+
 }
